1e742b905303ddcd93fafe84beceb18504460144
respectable2007 <zhanghui8876@163.com>
2019-03-05 10:55:53 +0800

    css modules

--- cssmodules/webpack.config.js
+++ cssmodules/webpack.config.js
@@ -1,8 +1,33 @@
 const path = require('path');
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
-  entry: 'src/index.js',
+  entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    path: path.resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: [
+        { loader: miniCssExtractPlugin.loader,
+          options: {
+            publicPath: './build/style'
+          } 
+        },
+        { loader: 'css-loader',
+          options: {
+            modules: true,//开启css modules功能
+            localIdentName: '[name]__[local]--[hash:base64:5]'
+          }
+        }
+      ]
+    }]
+  },
+  plugins: [
+    new miniCssExtractPlugin({
+      filename:'[name].css',
+      chunkFilename: '[id].css'
+    })
+  ]
 }
