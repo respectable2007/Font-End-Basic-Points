1bcd61cc7f5602230eebfc7046857bd7fbcfb344
respectable2007 <zhanghui8876@163.com>
2019-03-05 16:40:50 +0800

    css modules composes

--- cssmodules/src/index.js
+++ cssmodules/src/index.js
@@ -1,7 +1,7 @@
 import index from './style/index.css';
 import './style/global.css'
-var html = `<div class=${index.header}>
-    <h2 class=${index.title}>CSS Modules</h2>
+var html = `<div class="${index.header}">
+    <h2 class="${index.title}">CSS Modules</h2>
 </div>`
 
 document.getElementById('container').innerHTML = html;
